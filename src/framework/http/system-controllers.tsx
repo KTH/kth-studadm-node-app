4605f45024eebbea8ea1653bbff9a9e472d8eae1
Jon Larsson <jontelar@kth.se>
2018-08-21 09:48:04 +0200

    move errorMessages

--- src/framework/http/system-controllers.tsx
+++ src/framework/http/system-controllers.tsx
@@ -13,6 +13,7 @@ import { cortinaBlocksGetter } from '../cortina/get-cortina-blocks'
 import { ResourceFileNames } from '../pages/get-resource-filenames'
 import { ErrorPage } from '../pages/error-page'
 import { AboutPage, BuildInfo, PackageInfo } from '../pages/about-page'
+import { errorMessages } from '../i18n'
 
 export interface CortinaInput {
   lang: 'sv' | 'en'
@@ -21,25 +22,6 @@ export interface CortinaInput {
 }
 
 type ErrorControllerInput = CortinaInput & { error: any }
-
-export const errorMessages = {
-  errorNotFound: {
-    sv: 'Tyvärr kunde vi inte hitta sidan du söker',
-    en: 'Sorry, we can\'t find your requested page'
-  },
-  errorGeneric: {
-    sv: 'Något gick fel, var god försök igen senare',
-    en: 'Something went wrong, please try again later.'
-  },
-  errorForbidden: {
-    sv: 'Du saknar behörighet till sidan',
-    en: 'You don\'t have permission to access to this page'
-  },
-  supportReferenceId: {
-    sv: 'Vid felanmälan och kontakt med support, ange referens',
-    en: 'Please use this reference if you need support regarding this error'
-  }
-}
 
 function getFriendlyErrorMessage (error: any, lang: 'sv' | 'en') {
   const statusCode = error && error.status || 500
---
+++ src/framework/i18n/error-messages.ts
@@ -0,0 +1,18 @@
+export const errorMessages = {
+  errorNotFound: {
+    sv: 'Tyvärr kunde vi inte hitta sidan du söker',
+    en: 'Sorry, we can\'t find your requested page'
+  },
+  errorGeneric: {
+    sv: 'Något gick fel, var god försök igen senare',
+    en: 'Something went wrong, please try again later.'
+  },
+  errorForbidden: {
+    sv: 'Du saknar behörighet till sidan',
+    en: 'You don\'t have permission to access to this page'
+  },
+  supportReferenceId: {
+    sv: 'Vid felanmälan och kontakt med support, ange referens',
+    en: 'Please use this reference if you need support regarding this error'
+  }
+}
