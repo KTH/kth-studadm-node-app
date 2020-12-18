01fa60467efd7204181ded432529d1145c63a320
Stefan Sundkvist <sst@kth.se>
2020-12-18 15:48:56 +0100

    Fix: Example got broken after upgrading to latest express

--- src/framework/http/express/request-parsing.ts
+++ src/framework/http/express/request-parsing.ts
@@ -1,9 +1,8 @@
-import { Application } from 'express'
-import bodyParser from 'body-parser'
+import express, { Application } from 'express'
 import cookieParser from 'cookie-parser'
 
 export function applyRequestParsing (application: Application) {
-  application.use(bodyParser.json())
-  application.use(bodyParser.urlencoded({ extended: true }))
+  application.use(express.json())
+  application.use(express.urlencoded({ extended: true }))
   application.use(cookieParser())
 }
